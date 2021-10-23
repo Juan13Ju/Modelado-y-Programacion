0d4b56c35e76d0a98b558a8b221ffb95b6013423
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-10-23 13:23:18 -0500

    Update EstadoCocinando.java
    Modelos del robot

--- Practica02_Juarez_Mejia/src/EstadoCocinando.java
+++ Practica02_Juarez_Mejia/src/EstadoCocinando.java
@@ -11,18 +11,38 @@ class EstadoCocinando implements EstadoRobot{
     public void activar(){
 
         System.out.println("El robot ya esta activo y cocinando");
+        System.out.println("   .--,--.");
+        System.out.println("   `.  ,.'");
+        System.out.println("    |___|");
+        System.out.println("  |-:o o:-|   Si no son enchiladas");
+        System.out.println("   _`~^~'_          joven");
+        System.out.println(" |'   ^   `|");
     }
 
     @Override
     public void caminar(){
 
         System.out.println("El robot no puede caminar mientras cocina");
+        System.out.println("   .--,--.");
+        System.out.println("   `.  ,.'");
+        System.out.println("    |___|");
+        System.out.println("  |-:o o:-|  ¿Acaso quieres que te");
+        System.out.println("   _`~^~'_    queme con el aceite?");
+        System.out.println(" |'   ^   `|");
     }
 
     @Override
     public void atender(){
 
         System.out.println("La orden ya ha sido recibida, presione cocinar para empezar el proceso de cocina");
+        System.out.println("      \_/");
+        System.out.println("     (* *)    En seguida le traemos ");
+        System.out.println("    __) (__       su Mcburger ");
+        System.out.println("   ( )...( )(_)");
+        System.out.println("   || |_| ||//");
+        System.out.println(">==() | | ()/");
+        System.out.println("    _(___)_");
+        System.out.println("   [-]   [-]");
     }
 
     @Override
