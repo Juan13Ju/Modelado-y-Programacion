7b79608c84fd20f8a9f2eff61e035ea2c678e752
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-10-23 13:44:54 -0500

    Corregido

--- Practica02_Juarez_Mejia/src/EstadoCaminando.java
+++ Practica02_Juarez_Mejia/src/EstadoCaminando.java
@@ -15,28 +15,21 @@
     public void activar(){
 
         System.out.println("El robot ya esta activado y caminando hacia el cliente");
-        System.out.println("    \_/");
-	       System.out.println("   (_**)    ¡Voy corriendo!");
-       	System.out.println("  __) (_");
-	       System.out.println(" ( )...()");
-	       System.out.println(" || | |||");
-	       System.out.println(" || | |()__/");
-	       System.out.println(" /\(___)");
-	       System.out.println(" [-]   [-]");
+
     }
 
     @Override
     public void caminar(){
 
         System.out.println("El robot ya esta caminando hacia el cliente, presionar atender");
-     	  System.out.println("      \_/");
-	       System.out.println("     (* *)     Bienvenido a ");
-	       System.out.println("    __) (__       Mcburger ");
-	       System.out.println("   ( )...( )(_)");
-	       System.out.println("   || |_| ||//");
-	       System.out.println(">==() | | ()/");
-	       System.out.println("    _(___)_");
-	       System.out.println("   [-]   [-]");
+	System.out.println("    |_|");
+	System.out.println("   (_**)    ¡Voy corriendo!");
+	System.out.println("  __) (_");
+	System.out.println(" ( )...()");
+	System.out.println(" || | |||");
+	System.out.println(" || | |()__/");
+	System.out.println(" /|(___)");
+	System.out.println(" [-]   [-]");
     }
 
     /**
@@ -47,14 +40,14 @@
     public void atender(){
 
         System.out.println("El robot ha llegado al cliente, por favor presiones atender de nuevo para mostrarle el menu");
-        System.out.println("      \_/");
-	       System.out.println("     (* *)     ¿Que desear ");
-	       System.out.println("    __) (__       ordenar? ");
-	       System.out.println("   ( )...( )(_)");
-	       System.out.println("   || |_| ||//");
-	       System.out.println(">==() | | ()/");
-	       System.out.println("    _(___)_");
-	       System.out.println("   [-]   [-]");
+        System.out.println("      |_|");
+	System.out.println("     (* *)        ¿Que desea ordenar?");
+	System.out.println("    __) (__");
+	System.out.println("   ( )...( )(_)");
+	System.out.println("   || |_| ||//");
+	System.out.println(">==() | | ()/");
+	System.out.println("    _(___)_");
+	System.out.println("   [-]   [-]");
 
         robot.setState(robot.getEstadoAtendiendo());
     }
@@ -63,14 +56,14 @@
     public void cocinar(){
 
         System.out.println("Cocinar mientras el robot camina es peligroso! en cuanto el robot llegue lo atendera");
-     	  System.out.println("    \_/");
-	       System.out.println("   (_**)   O lo atiendo o cocino?");
-	       System.out.println("  __) (_   no se puede ambas joven");
-	       System.out.println(" ( )...()");
-	       System.out.println(" || | |||");
-	       System.out.println(" || | |()__/");
-	       System.out.println(" /\(___)");
-	       System.out.println(" [-]   [-]");
+     	System.out.println("    |_|");
+	System.out.println("   (_**)   O lo atiendo o cocino?");
+	System.out.println("  __) (_   no se puede ambas joven");
+	System.out.println(" ( )...()");
+	System.out.println(" || | |||");
+	System.out.println(" || | |()__/");
+	System.out.println(" /|(___)");
+	System.out.println(" [-]   [-]");
 
      
     }
@@ -79,14 +72,14 @@
     public void suspender(){
 
         System.out.println("Suspendiendo el robot... \n 3... \n 2... \n 1... \n Robot suspendido");
-     	  System.out.println("      \_/");
-	       System.out.println("     (* *)   A mimir again");
-	       System.out.println("    __) (__");
-	       System.out.println("   ( )...( )(_)");
-	       System.out.println("   || |_| ||//");
-	       System.out.println(">==() | | ()/");
-	       System.out.println("    _(___)_");
-	       System.out.println("   [-]   [-]");
+     	System.out.println("      |_|");
+	System.out.println("     (* *)   A mimir again");
+	System.out.println("    __) (__");
+	System.out.println("   ( )...( )(_)");
+	System.out.println("   || |_| ||//");
+	System.out.println(">==() | | ()/");
+	System.out.println("    _(___)_");
+	System.out.println("   [-]   [-]");
         robot.setState(robot.getEstadoSuspendido());
     }
  }
