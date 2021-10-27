b79543d0962e716737ec26908b193f5af40c86e6
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-10-27 17:33:04 -0500

    Update EstadoAtendiendo.java

--- Practica02_Juarez_Mejia/src/EstadoAtendiendo.java
+++ Practica02_Juarez_Mejia/src/EstadoAtendiendo.java
@@ -1,6 +1,7 @@
 /**
  * Estado en el cual el robot se encuentra habilitado para atender, por lo cual solo se puede activar.
- * Para ayudar al usuario a familiarizarse con el modo de activaciones se ha agregado un modelo de un robot en ascii.
+ * Para ayudar al usuario a familiarizarse con el modo de activaciones se ha agregado un modelo de un robot en ascii
+ * el cual ilustra cada uno de los estados posibles del robot.
  */
 
 import java.util.Scanner;
@@ -47,6 +48,14 @@ class EstadoAtendiendo implements EstadoRobot{
     public void atender(){
 
         System.out.println("Comenzando a leer el menu");
+	System.out.println("      |_|     ____");
+	System.out.println("     (* *)   |menu|");
+	System.out.println("    __) (__  |____|");
+	System.out.println("   ( )...( )(_)");
+	System.out.println("   || |_| ||//");
+	System.out.println(">==() | | ()/");
+	System.out.println("    _(___)_");
+	System.out.println("   [-]   [-]");
         robot.printMenu();
 
         Scanner scanner = new Scanner(System.in);
@@ -71,10 +80,27 @@ class EstadoAtendiendo implements EstadoRobot{
     public void cocinar(){
 
         System.out.println("El robot no puede cocinar sin una orden recibida");
+	System.out.println("   .--,--.");
+        System.out.println("   `.  ,.'");
+        System.out.println("    |___|");
+        System.out.println("  |-:o o:-|   Si no soy adivino");
+        System.out.println("   _`~^~'_          joven");
+        System.out.println(" |'   ^   `|");
+	
     }
 
     public void suspender(){
 
         System.out.println("El robot no se puede suspender mientras atiende a un cliente");
+	System.out.println("¿Quiere agregar    (___ )");
+	System.out.println(" papas a su orden?  oo~)/");
+	System.out.println("    |_|             |o_|");
+	System.out.println("   (_**)          / (|)  - ");
+	System.out.println("  __) (_         ( / .- ) )");
+	System.out.println(" ( )...()        | | .  /_/");
+	System.out.println(" || | |||         (|___(_/");
+	System.out.println(" || | |()__/       | |  |");
+	System.out.println(" /|(___)           |_|__|");
+	System.out.println(" [-]   [-]        (_(___]");
     }
 }
