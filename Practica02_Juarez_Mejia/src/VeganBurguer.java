069b9ec0eab0fdf04bff4faba95e0ccb5c12b664
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-10-27 22:47:53 -0500

    Update VeganBurguer.java

--- Practica02_Juarez_Mejia/src/VeganBurguer.java
+++ Practica02_Juarez_Mejia/src/VeganBurguer.java
@@ -1,5 +1,11 @@
+/**
+ * Sub clase que define las caracteristicas de las hamburguesas de MacBurguesa que son veganas/vegetarianas
+ */
 class VeganBurguer extends Burguer{
 
+    /**
+     * Constructor para la hamburguesa con carne de soya como alternativa para veganos y vegetarianos
+     */
     public VeganBurguer(Macburguesa ham){
         super(ham);
     }
