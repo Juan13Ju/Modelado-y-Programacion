10b26efec98fa83d2259da55e361f03d5680e353
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-10-27 22:37:49 -0500

    Update Robot.java

--- Practica02_Juarez_Mejia/src/Robot.java
+++ Practica02_Juarez_Mejia/src/Robot.java
@@ -1,19 +1,38 @@
+/**
+ * Clase que define al robot encargado de Macburgers
+ */
 class Robot{
 
     // Atributo para saber si el robot recibio la orden del cliente para comenzar a cocinar
     public Macburguesa orden;
-
+    
+    // Atributo para saber el estado del robot 
     private EstadoRobot estadoActual;
 
+    // Atributo para saber el menu del dia
     private Menu menuDelDia;
+    
+    // Atributo para saber el menu general
     private Menu menuGeneral;
+    
+    // Atributo para saber el menu de lujo
     private Menu menuDeLujo;
 
+    // Atributo para saber si el robot esta en estado suspendido
     private EstadoRobot estadoSuspendido;
+    
+    // Atributo para saber si el robot esta en estado caminando
     private EstadoRobot estadoCaminando;
+    
+    // Atributo para saber si el robot esta en estado atendiendo
     private EstadoRobot estadoAtendiendo;
+    
+    // Atributo para saber si el robot esta en estado cocinando
     private EstadoRobot estadoCocinando;
 
+    /**
+     * Constructor para el robot
+     */
     public Robot(){
 
         estadoSuspendido = new EstadoSuspendido(this);
@@ -29,51 +48,82 @@ class Robot{
         this.menuDeLujo = new MenuDeLujo();
     }
 
+    /**
+    * Regresa el estado del robot y nos dice si se encuentra en estado suspendido
+    */
     public EstadoRobot getEstadoSuspendido(){
 
         return this.estadoSuspendido;
     }
 
+    /**
+    * Regresa el estado del robot y nos dice si se encuentra en estado caminando
+    */
     public EstadoRobot getEstadoCaminando(){
 
         return this.estadoCaminando;
     }
 
+    /**
+    * Regresa el estado del robot y nos dice si se encuentra en estado atendiendo
+    */
     public EstadoRobot getEstadoAtendiendo(){
 
         return this.estadoAtendiendo;
     }
 
+    /**
+    * Regresa el estado del robot y nos dice si se encuentra en estado cocinando
+    */
     public EstadoRobot getEstadoCocinando(){
 
         return this.estadoCocinando;
     }
 
+    /**
+    * Modifica el estado del robot
+    */
     public void setState(EstadoRobot nuevoEstado){
 
         this.estadoActual = nuevoEstado;
     }
 
+    /**
+     * Metodo para pasar el estado del robot a activar
+     */
     public void activar(){
 
         estadoActual.activar();
     }
 
+    /**
+     * Metodo para pasar el estado del robot a caminar
+     */
     public void caminar(){
 
         estadoActual.caminar();
     }
 
+    /**
+     * Metodo para pasar el estado del robot a atender
+     */
     public void atender(){
 
         estadoActual.atender();
     }
 
+    /**
+     * Metodo para pasar el estado del robot a cocinar
+     */
     public void cocinar(){
 
         estadoActual.cocinar();
     }
 
+    /**
+     * Metodo para pasar el estado del robot a cocinar una hamburguesa de acuerdo a su categoria vegetariana o no
+     *@param ham
+     */
     public void cocinar(Macburguesa ham){
 
         Burguer mac;
@@ -85,13 +135,16 @@ class Robot{
         }
     }
 
+    /**
+     * Metodo para pasar el estado del robot a suspendido
+     */
     public void suspender(){
 
         estadoActual.suspender();
     }
 
     /**
-     * Metodo para imprimir el menu del restaurant
+     * Metodo para imprimir el menu del restaurante
      */
     public void printMenu(){
 
@@ -124,6 +177,10 @@ class Robot{
     }
 
 
+    /**
+     * Metodo para buscar los productos de un menu especifico usando su id
+     * @param id 
+     */
     public void findBurguer(String id) throws InvalidIdException{
 
         MyIterator generalIterator = menuGeneral.createIterator();
