ce263a32379d51793e74d8ac0232b2f750291d5b
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-04 21:40:05 -0600

    Update Jamon.java

--- Practica03Juarez_Mejia/src/Jamon.java
+++ Practica03Juarez_Mejia/src/Jamon.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa al jamon como ingrediente de una baguette
+ */
 class Jamon extends BaguetteDecorator{
 
     Baguette bg;
@@ -7,12 +10,18 @@ class Jamon extends BaguetteDecorator{
         this.bg = bg;
     }
 
+/**
+ * Metodo que da la descripcion del ingrediente
+ */
     @Override
     public String getDescription(){
 
         return bg.getDescription() + ", jamon";
     }
 
+/**
+ * Metodo que da el precio del ingrediente
+ */
     @Override
     public double costo(){
 
