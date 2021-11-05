26917e1651acbe982f95dfedd95d1fe6179b3587
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-04 21:42:26 -0600

    Create Jitomate.java

---
+++ Practica03Juarez_Mejia/src/Jitomate.java
@@ -0,0 +1,30 @@
+/**
+ * Clase que representa al jitomate como ingrediente de una baguette
+ */
+class Pollo extends BaguetteDecorator{
+    
+    Baguette bg;
+
+    public Jitomate(Baguette bg){
+
+        this.bg = bg;
+    }
+
+/**
+ * Metodo que da la descripcion del ingrediente
+ */
+    @Override
+    public String getDescription(){
+
+        return bg.getDescription() + " , jitomate";
+    }
+    
+/**
+ * Metodo que da el precio del ingrediente
+ */
+    @Override
+    public double costo(){
+        
+        return bg.costo() + 1.5;
+    }
+}
