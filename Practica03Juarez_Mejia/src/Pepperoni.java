bea5495b7c836bda4e26edad5d979cf21988623f
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-04 21:39:12 -0600

    Update Pepperoni.java

--- Practica03Juarez_Mejia/src/Pepperoni.java
+++ Practica03Juarez_Mejia/src/Pepperoni.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa al pepperoni como ingrediente de una baguette
+ */
 class Pepperoni extends BaguetteDecorator{
 
     Baguette bg;
@@ -7,12 +10,18 @@ class Pepperoni extends BaguetteDecorator{
         this.bg = bg;
     }
 
+/**
+ * Metodo que da la descripcion del ingrediente
+ */
     @Override
     public String getDescription(){
 
         return bg.getDescription() + ", pepperoni";
     }
-
+    
+/**
+ * Metodo que da el precio del ingrediente
+ */
     @Override
     public double costo(){
 
