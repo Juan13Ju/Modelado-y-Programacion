f74a7831636c4ead3ef209679dab29ab556d157a
juan13ju <juan13ubaldo@gmail.com>
2021-11-12 19:40:22 -0600

    Clase que representa el componente de un carro para no repetir la parte de ataque, defensa, etc

---
+++ Practica04Juarez_Mejia/src/ComponenteCarro.java
@@ -0,0 +1,36 @@
+/**
+ * Clase abtsracta que representa el componente de un carro para evitar
+ * repetir en cada clase (Llanta, Blindaje, etc) los atributos en comun de los componentes
+ */
+
+public abstract class ComponenteCarro{
+
+    private int ataque;
+    private int defensa;
+    private int velocidad;
+    private int costo;
+
+    public ComponenteCarro(int ataque, int defensa, int velocidad, int costo){
+        this.ataque = ataque;
+        this.defensa = defensa;
+        this.velocidad = velocidad;
+        this.costo = costo;
+    }
+
+    // Metodos setters
+    public int getAtaque(){
+        return this.ataque;
+    }
+
+    public int getDefensa(){
+        return this.defensa;
+    }
+
+    public int getVelocidad(){
+        return this.velocidad;
+    }
+
+    public int getCosto(){
+        return this.costo;
+    }
+}
