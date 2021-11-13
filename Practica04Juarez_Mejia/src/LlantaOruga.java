d0920e013bf9639b069b8625ceea6e1d95f7f1b9
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-12 22:18:31 -0600

    Create LlantaOruga.java

---
+++ Practica04Juarez_Mejia/src/LlantaOruga.java
@@ -0,0 +1,25 @@
+
+class LlantaOruga extends ComponenteCarro implements Llanta{
+
+    String tipo = " ";
+
+    public LlantaOruga(){
+        // Aqui guardamos los atributos de ataque, defensa, velocidad y costo de la llanta
+        super(70, 90, 36, 100);
+    }
+
+    /**
+     * Indicamos el tipo de llanta que es para despues poder imprimirlo
+     */
+    @Override
+    public void crearLlanta(){
+        tipo = "Llanta oruga de tanque";
+    }
+
+    @Override
+    public void getTipo(){
+        System.out.println(tipo);
+    }
+
+
+}
