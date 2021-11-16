8afdae4787c72f70a218226510bbe5028ad931f9
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:25:39 -0600

    Update BlindajeTanque.java

--- Practica04Juarez_Mejia/src/BlindajeTanque.java
+++ Practica04Juarez_Mejia/src/BlindajeTanque.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa a BlindajeTanque como un componente de carro y un tipo de blindaje
+ */
 public class BlindajeTanque extends ComponenteCarro implements Blindaje{
 
     String tipo = " ";
@@ -7,12 +10,18 @@ public class BlindajeTanque extends ComponenteCarro implements Blindaje{
         super(46, 84, 14, 89);
     }
 
+ /**
+ * Metodo que crea un blindaje del tipo tanque
+ */
     @Override
     public void crearBlindaje(){
 
         tipo = "Blindaje tanque";
     }
-
+    
+/**
+ * Metodo que da el tipo de blindaje
+ */
     @Override
     public void getTipo(){
 
