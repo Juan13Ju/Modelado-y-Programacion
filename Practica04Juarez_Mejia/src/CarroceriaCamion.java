2da89591e11a6357cd3205df0d8bddd627c56367
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:35:16 -0600

    Update CarroceriaCamion.java

--- Practica04Juarez_Mejia/src/CarroceriaCamion.java
+++ Practica04Juarez_Mejia/src/CarroceriaCamion.java
@@ -1,19 +1,27 @@
+/**
+ * Clase que representa a carroceriaCamion como un componente de carro y un tipo de carroceria
+ */
 public class CarroceriaCamion extends ComponenteCarro implements Carroceria{
 
     String tipo = " ";
 
     public CarroceriaCamion(){
-
-        // Los atributos ataque, defensa, velocidad y costo de la carroceria
+        // Los atributos ataque, defensa, velocidad y costo de la carroceriaCamion
         super(30, 45, 38, 80);
     }
-
+    
+/**
+ * Metodo que crea una carroceria del tipo camion
+ */
     @Override
     public void crearCarroceria(){
 
         tipo = "Carroceria camion";
     }
-
+    
+/**
+ * Metodo que da el tipo de carroceria
+ */
     @Override
     public void getTipo(){
 
