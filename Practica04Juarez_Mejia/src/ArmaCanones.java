0444e74aadf23e0b1356784e0b70e42b14a3a4f4
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:10:51 -0600

    Update ArmaCanones.java

--- Practica04Juarez_Mejia/src/ArmaCanones.java
+++ Practica04Juarez_Mejia/src/ArmaCanones.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa a armaCanones como un componente de carro y un tipo de arma
+ */
 public class ArmaCanones extends ComponenteCarro implements Arma{
 
     String tipo = " ";
@@ -6,11 +9,17 @@ public class ArmaCanones extends ComponenteCarro implements Arma{
         super(80, 12, 60, 59);
     }
 
+/**
+ * Metodo que crea un arma del tipo cañon
+ */
     @Override
     public void crearArma(){
         tipo = "Cañones";
     }
 
+/**
+ * Metodo que da el tipo de arma
+ */
     @Override
     public void getTipo(){
         System.out.println(tipo);
