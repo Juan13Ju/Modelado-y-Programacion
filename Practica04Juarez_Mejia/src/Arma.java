e2e779c0b95462e663f81543dd7b1c4d55408d7e
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:01:55 -0600

    Update Arma.java

--- Practica04Juarez_Mejia/src/Arma.java
+++ Practica04Juarez_Mejia/src/Arma.java
@@ -1,6 +1,15 @@
+/**
+ * Clase abstracta que representa un arma
+ */
 public interface Arma{
 
+ /**
+ * Metodo que crea un arma
+ */
     public void crearArma();
 
+/**
+ * Metodo que da el tipo de arma
+ */
     public void getTipo();
 }
