7d8ecf613254c16fe4fd8706b56aabf19b530076
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:17:20 -0600

    Update ArmaSierra.java

--- Practica04Juarez_Mejia/src/ArmaSierra.java
+++ Practica04Juarez_Mejia/src/ArmaSierra.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa a armaSierra como un componente de carro y un tipo de arma
+ */
 public class ArmaSierra extends ComponenteCarro implements Arma{
 
     String tipo = " ";
@@ -6,11 +9,17 @@ public class ArmaSierra extends ComponenteCarro implements Arma{
         super(30, 0, 70, 12);
     }
 
+/**
+ * Metodo que crea un arma del tipo sierra
+ */
     @Override
     public void crearArma(){
         tipo = "Sierra";
     }
 
+/**
+ * Metodo que da el tipo de arma
+ */
     @Override
     public void getTipo(){
         System.out.println(tipo);
