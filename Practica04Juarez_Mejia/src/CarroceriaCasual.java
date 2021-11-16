1481566c31f238063be3157b66e255ee0b371623
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:37:27 -0600

    Update CarroceriaCasual.java

--- Practica04Juarez_Mejia/src/CarroceriaCasual.java
+++ Practica04Juarez_Mejia/src/CarroceriaCasual.java
@@ -1,19 +1,28 @@
+/**
+ * Clase que representa a carroceriaCasual como un componente de carro y un tipo de carroceria
+ */
 public class CarroceriaCasual extends ComponenteCarro implements Carroceria{
 
     String tipo = " ";
 
     public CarroceriaCasual(){
 
-        // Los atributos ataque, defensa, velocidad y costo de la carroceria
+        // Los atributos ataque, defensa, velocidad y costo de la carroceriaCasual
         super(10, 20, 30, 40);
     }
-
+    
+/**
+ * Metodo que crea una carroceria del tipo casual
+ */
     @Override
     public void crearCarroceria(){
 
         tipo = "Carroceria casual";
     }
-
+    
+/**
+ * Metodo que da el tipo de carroceria
+ */
     @Override
     public void getTipo(){
 
