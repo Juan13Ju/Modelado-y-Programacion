f5355839f4bd455da7ef935c5c2b15ead90de395
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:12:09 -0600

    Update ArmaLanzallamas.java

--- Practica04Juarez_Mejia/src/ArmaLanzallamas.java
+++ Practica04Juarez_Mejia/src/ArmaLanzallamas.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa a armaLanzallamas como un componente de carro y un tipo de arma
+ */
 public class ArmaLanzallamas extends ComponenteCarro implements Arma{
 
     String tipo = " ";
@@ -6,11 +9,17 @@ public class ArmaLanzallamas extends ComponenteCarro implements Arma{
         super(60, 78, 90, 85);
     }
 
+ /**
+ * Metodo que crea un arma del tipo lanzallamas
+ */
     @Override
     public void crearArma(){
         tipo = "Lanzallamas";
     }
 
+/**
+ * Metodo que da el tipo de arma
+ */
     @Override
     public void getTipo(){
         System.out.println(tipo);
