ca3bddb75bbb388ab3fd273c1276a3ecfdf12f7e
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:26:58 -0600

    Update Carroceria.java

--- Practica04Juarez_Mejia/src/Carroceria.java
+++ Practica04Juarez_Mejia/src/Carroceria.java
@@ -1,6 +1,15 @@
+/**
+ * Clase que representa una carroceria
+ */
 public interface Carroceria{
 
+/**
+ * Metodo que crea una carroceria
+ */
     public void crearCarroceria();
-
+    
+/**
+ * Metodo que da el tipo de carroceria
+ */
     public void getTipo();
 }
