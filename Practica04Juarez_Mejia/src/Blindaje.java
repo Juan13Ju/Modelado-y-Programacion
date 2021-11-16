164da9eea4889876008469e103a1865fcf3d3515
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:21:36 -0600

    Update Blindaje.java

--- Practica04Juarez_Mejia/src/Blindaje.java
+++ Practica04Juarez_Mejia/src/Blindaje.java
@@ -1,6 +1,15 @@
+/**
+ * Clase que representa un blindaje
+ */
 public interface Blindaje{
 
+/**
+ * Metodo que crea un blindaje
+ */
     public void crearBlindaje();
 
+/**
+ * Metodo que da el tipo de blindaje
+ */
     public void getTipo();
 }
