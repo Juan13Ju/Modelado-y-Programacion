0d3112b48ab411958f29e2b285700810f1e4fc60
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:51:57 -0600

    Update LlantaDeportiva.java

--- Practica04Juarez_Mejia/src/LlantaDeportiva.java
+++ Practica04Juarez_Mejia/src/LlantaDeportiva.java
@@ -1,20 +1,26 @@
+/**
+ * Clase que representa a LlantaDeportiva como un componente de carro y un tipo de llanta
+ */
 public class LlantaDeportiva extends ComponenteCarro implements Llanta{
 
     String tipo = " ";
 
     public LlantaDeportiva(){
-        // Aqui guardamos los atributos de ataque, defensa, velocidad y costo de la llanta
+        // Los atributos de ataque, defensa, velocidad y costo de la llanta
         super(35, 15, 90, 60);
     }
 
-    /**
-     * Indicamos el tipo de llanta que es para despues poder imprimirlo
-     */
+ /**
+ * Metodo que crea una llanta del tipo deportiva
+ */
     @Override
     public void crearLlanta(){
         tipo = "Llanta deportiva";
     }
-
+    
+/**
+ * Metodo que da el tipo de llanta
+ */
     @Override
     public void getTipo(){
         System.out.println(tipo);
