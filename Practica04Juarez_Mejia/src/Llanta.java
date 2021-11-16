ed8c12615ef9a254e4f70813ae0736228d90a9d4
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:48:02 -0600

    Update Llanta.java

--- Practica04Juarez_Mejia/src/Llanta.java
+++ Practica04Juarez_Mejia/src/Llanta.java
@@ -1,6 +1,15 @@
+/**
+ * Clase que representa una llanta
+ */
 public interface Llanta{
-
+    
+ /**
+ * Metodo que crea una llanta
+ */
     public void crearLlanta();
-
+    
+/**
+ * Metodo que da el tipo de llanta
+ */
     public void getTipo();
 }
