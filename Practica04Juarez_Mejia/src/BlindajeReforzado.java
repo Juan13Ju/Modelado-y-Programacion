d95f0ea5d4e7d5c3a996111b69afc4eca3570f26
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:23:22 -0600

    Update BlindajeReforzado.java

--- Practica04Juarez_Mejia/src/BlindajeReforzado.java
+++ Practica04Juarez_Mejia/src/BlindajeReforzado.java
@@ -1,3 +1,6 @@
+/**
+ * Clase que representa a BlindajeReforzado como un componente de carro y un tipo de blindaje
+ */
 public class BlindajeReforzado extends ComponenteCarro implements Blindaje{
 
     String tipo = " ";
@@ -6,13 +9,19 @@ public class BlindajeReforzado extends ComponenteCarro implements Blindaje{
 
         super(34, 43, 27, 70);
     }
-
+    
+/**
+ * Metodo que crea un blindaje del tipo reforzado
+ */
     @Override
     public void crearBlindaje(){
 
         tipo = "Blindaje reforzado";
     }
-
+    
+/**
+ * Metodo que da el tipo de blindaje
+ */
     @Override
     public void getTipo(){
 
