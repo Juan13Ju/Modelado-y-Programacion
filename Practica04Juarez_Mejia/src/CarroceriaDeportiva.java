40dc533341fc87a40f98cd8451fd62e2756ba259
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 17:38:34 -0600

    Update CarroceriaDeportiva.java

--- Practica04Juarez_Mejia/src/CarroceriaDeportiva.java
+++ Practica04Juarez_Mejia/src/CarroceriaDeportiva.java
@@ -1,19 +1,28 @@
+/**
+ * Clase que representa a carroceriaDeportiva como un componente de carro y un tipo de carroceria
+ */
 public class CarroceriaDeportiva extends ComponenteCarro implements Carroceria{
 
     String tipo = " ";
 
     public CarroceriaDeportiva(){
 
-        // Los atributos ataque, defensa, velocidad y costo de la carroceria
+        // Los atributos ataque, defensa, velocidad y costo de la carroceriaDeportiva
         super(15, 15, 45, 66);
     }
-
+    
+/**
+ * Metodo que crea una carroceria del tipo camion
+ */
     @Override
     public void crearCarroceria(){
 
         tipo = "Carroceria deportiva";
     }
-
+    
+/**
+ * Metodo que da el tipo de carroceria
+ */
     @Override
     public void getTipo(){
 
