0759232a11cb864cd7c2fb04b42d9aa4ea120b46
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 18:10:00 -0600

    Update MotorTurbo.java

--- Practica04Juarez_Mejia/src/MotorTurbo.java
+++ Practica04Juarez_Mejia/src/MotorTurbo.java
@@ -1,18 +1,27 @@
+/**
+ * Clase que representa a MotorTurbo como un componente de carro y un tipo de motor
+ */
 public class MotorTurbo extends ComponenteCarro implements Motor{
 
     String tipo = " ";
 
     public MotorTurbo(){
-        // Los atributos del motor, ataque, defensa, velocidad y costo
+        // Los atributos de ataque, defensa, velocidad y costo del MotorTurbo
         super(46, 34, 68, 45);
     }
-
+    
+/**
+ * Metodo que crea un motor del tipo turbo
+ */
     @Override
     public void crearMotor(){
 
         tipo = "Motor turbo";
     }
-
+    
+/**
+ * Metodo que da el tipo de motor
+ */
     @Override
     public void getTipo(){
 
