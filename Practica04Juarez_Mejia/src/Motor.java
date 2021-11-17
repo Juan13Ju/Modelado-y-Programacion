ba6b423f252979b3bc14d213f41b88bb230b034a
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 18:01:56 -0600

    Update Motor.java

--- Practica04Juarez_Mejia/src/Motor.java
+++ Practica04Juarez_Mejia/src/Motor.java
@@ -1,6 +1,15 @@
+/**
+ * Clase que representa un motor
+ */
 interface Motor{
-
+    
+/**
+ * Metodo que crea un motor
+ */
     public void crearMotor();
-
+    
+/**
+ * Metodo que da el tipo de motor
+ */
     public void getTipo();
 }
