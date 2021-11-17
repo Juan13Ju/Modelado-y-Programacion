730f8deee08bab06f64cbb7f07813b2c330c69f4
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 18:06:41 -0600

    Update MotorDiesel.java

--- Practica04Juarez_Mejia/src/MotorDiesel.java
+++ Practica04Juarez_Mejia/src/MotorDiesel.java
@@ -1,18 +1,27 @@
+/**
+ * Clase que representa a MotorDiesel como un componente de carro y un tipo de motor
+ */
 public class MotorDiesel extends ComponenteCarro implements Motor{
 
     String tipo = " ";
 
     public MotorDiesel(){
-        // Los atributos del motor, ataque, defensa, velocidad y costo
+        // Los atributos de ataque, defensa, velocidad y costo del motorDiesel
         super(33, 30, 50, 30);
     }
-
+    
+/**
+ * Metodo que crea un motor del tipo diesel
+ */
     @Override
     public void crearMotor(){
 
         tipo = "Motor diesel";
     }
-
+    
+/**
+ * Metodo que da el tipo de motor
+ */
     @Override
     public void getTipo(){
 
