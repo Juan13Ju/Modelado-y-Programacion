948e58f587915df12bcac50650c76efabb556848
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-11-16 18:04:40 -0600

    Update MotorDeportivo.java

--- Practica04Juarez_Mejia/src/MotorDeportivo.java
+++ Practica04Juarez_Mejia/src/MotorDeportivo.java
@@ -1,18 +1,27 @@
+/**
+ * Clase que representa a MotorDeportivo como un componente de carro y un tipo de arma
+ */
 public class MotorDeportivo extends ComponenteCarro implements Motor{
 
     String tipo = " ";
 
     public MotorDeportivo(){
-        // Los atributos del motor, ataque, defensa, velocidad y costo
+        // Los atributos de ataque, defensa, velocidad y costo del motorDeportivo
         super(25, 25, 80, 60);
     }
-
+    
+/**
+ * Metodo que crea un motor del tipo deportivo 
+ */
     @Override
     public void crearMotor(){
 
         tipo = "Motor deportivo";
     }
-
+    
+/**
+ * Metodo que da el tipo de motor
+ */
     @Override
     public void getTipo(){
 
