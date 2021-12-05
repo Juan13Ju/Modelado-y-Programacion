e5d741df8389b71d1a70b599f50ac33c6a2cb03a
ArmandoTL8 <42941929+ArmandoTL8@users.noreply.github.com>
2021-12-05 03:30:59 -0600

    Implemente metódos para poder usar la opcion 2

--- Proyecto01/src/Spain.java
+++ Proyecto01/src/Spain.java
@@ -59,6 +59,48 @@ public class Spain implements Lenguaje{
                 while(productos.hasNext()){
                     System.out.println(productos.next().toString());
                 }
+                    do{
+                        System.out.println("Escribe el codigo de barras del producto que deseais comprar\n");
+                        String productwanted = in.nextLine();
+                        Producto p = catalogo.getProducto(productwanted);
+                        option = productwanted;
+                        while(true){
+                        try{
+                        System.out.println("¿Quereis añadir: " + p.getNombre() + " a tu carrito de compras?\n"
+                                + "1.- Si tio\n"
+                                + "2.- No ostias\n");
+                        String want = in.nextLine();
+                        desc = Integer.parseInt(want);
+                        break;
+                        } catch (NumberFormatException ex){
+                            System.out.println("Ingresa una opcion valida\n"
+                                    + "¿Quereis añadir: " + p.getNombre() + " a tu carrito de compras??\n"
+                                            + "1.- Si tio\n"
+                                            + "2.- No ostias\n");
+                        }
+                        }
+                        if (desc == 1){
+                            carrito.add(p);
+                            while(true){
+                                try{
+                                    System.out.println("¿Quereis añadir mas productos a tu carrito de compras?\n"
+                                            + "1.- Si tio\n"
+                                            + "2.- No ostias\n");
+                                    String want = in.nextLine();
+                                    more = Integer.parseInt(want);
+                                    break;
+                                } catch (NumberFormatException ex){
+                                    System.out.println("¿Quereis añadir mas productos a tu carrito de compras?\n"
+                                            + "1.- Si tio\n"
+                                            + "2.- No ostias\n");
+                                }
+                            }
+                            if (more == 1){
+                                option = "continue";
+                            } else System.out.println("Falta codigo aqui");//aquí va el código cuando ya quiere realizar la compra
+                            option = "cancel"; 
+                                } 
+                    } while (option == "cancel");
             case 3:
                 break;
             default:
@@ -66,7 +108,6 @@ public class Spain implements Lenguaje{
                 break;
                 
         }
-    } while (opcion != 3);
     }
     
 }
