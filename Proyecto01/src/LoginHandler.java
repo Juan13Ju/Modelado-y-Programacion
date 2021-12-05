ef2ed51eb365654744485e673b83aefd48af6681
juan13ju <juan13ubaldo@gmail.com>
2021-12-05 00:09:38 -0600

    Ahora el login regresa un usuario y arroja un error en caso de credenciales incorrectas

--- Proyecto01/src/LoginHandler.java
+++ Proyecto01/src/LoginHandler.java
@@ -2,56 +2,28 @@
 
 import java.util.LinkedList;
 class LoginHandler{
-
     /**
      * Metodo para realizar el login de un usario
      * @param nombreUsuario El usuario que va a iniciar sesion
      * @param contrasena La contraseña ingresada por el usuario
-     * ? Seria mejor usar un HashMap que contenga como llave el nombre del usuario?
-     * ? Para evitar recorrer la lista. Que vamos a tener como 5 usuarios entonces no importa mucho xd
      * @param usuarios Una lista ligada que contiene a los usaurios de la aplicacion
-     * @return true si 
+     * @return El usuario logeado
+     * @throws WrongCredentialsException si no encontro al usuario o la contraseña es incorrecta
      */
-    public static boolean login(String nombreUsuario, String contrasena, LinkedList<Usuario>usuarios){
+    public static Usuario login(String nombreUsuario, String contrasena, LinkedList<Usuario>usuarios) throws WrongCredentialsException{
 
         // Recorremos la lista de usuarios hasta encontrar al usuario que va a hacer login
         for(Usuario user : usuarios){
             // Y regresamos si la contraseña que ingreso es igual a la contraseña guardada
             if(user.getUsuario().equals(nombreUsuario)){
-                return user.getContrasena().equals(contrasena);
+                if(user.getContrasena().equals(contrasena)){
+                    return user;
+                }else{
+                    throw new WrongCredentialsException();
+                }
             }
         }
 
-        return false;
+        throw new WrongCredentialsException();
     }
-    /**
-     * Método para recuperar la nacionalidad de un usuario dado su nombre y contraseña
-     * @param nombreUsuario
-     * @param contrasena
-     * @param usuarios
-     * @return 
-     */
-    public static String getNationality(String nombreUsuario, String contrasena, LinkedList<Usuario>usuarios){
-        for(Usuario user: usuarios){
-            if((user.getUsuario().equals(nombreUsuario)) && (user.getContrasena().equals(contrasena))){
-                return user.getPais();
-            }
-        }
-        return null;
-    }
-    /**
-     * Método para recuperar el usuario dado su nombre y contraseña
-     * @param nombreUsuario
-     * @param contrasena
-     * @param usuarios
-     * @return 
-     */
-    public static Usuario getUser(String nombreUsuario, String contrasena, LinkedList<Usuario>usuarios){
-        for(Usuario user: usuarios){
-            if((user.getUsuario().equals(nombreUsuario)) && (user.getContrasena().equals(contrasena))){
-                return user;
-            }
-        }
-        return null;
-    }
-}
+}  
