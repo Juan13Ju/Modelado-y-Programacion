a3bf0b083bc5634545e8f6056bd9bc80452e9d12
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-12-05 22:27:00 -0600

    Update Prueba.java

--- Proyecto1/src/icc/Prueba.java
+++ Proyecto1/src/icc/Prueba.java
@@ -8,23 +8,30 @@ private static final Random random = new Random();
 public static void main(final String[] args) {
 
     System.out.println("¡Bienvenido al Simulador para el Lanzamiento de Dados!");
-    System.out.println("¿De cuantos lanzamientos consiste tu simulacion?");
+    System.out.println("Teclea el numero correspondiente a la opcion deseada o cualquier otro n ́umero para cerrar el simulador:");
+
     int entrada = 0;
     try (Scanner scanner = new Scanner(System.in)) {
         entrada = scanner.nextInt();
+
+	
+    } catch(Exception e){
+	System.out.println("Gracias por utilizar el Simulador para el Lanzamiento de Dados. Hasta luego.");
     }
     
     final int[] simulacionDados = new int[11];
     for (int x = 0; x < entrada; x++) {
         simulacionDados[suma() - 2]++;
     }
-    
+    /*
     System.out.println(String.format("RESULTADOS DE LA SIMULACION"));
     for (int i = 0; i < 11; i++) {
         System.out.println(String.format("%s salio un total de %s veces ", i + 2, simulacionDados[i]));
     }
+    */
     
     // Para imprirmir el histograma
+    
     for(int i = 0; i < 11; i ++){
         System.out.print(i + 2 + ": ");
         for(int j = 0; j < simulacionDados[i]; j++){
