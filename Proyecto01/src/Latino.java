7085bdfce6d5633daf20ae09d1cb0036e26d4e4f
ArmandoTL8 <42941929+ArmandoTL8@users.noreply.github.com>
2021-12-05 23:54:21 -0600

    agregué las variables que me faltaban definir

--- Proyecto01/src/Latino.java
+++ Proyecto01/src/Latino.java
@@ -7,7 +7,12 @@ public class Latino implements Lenguaje{
     @Override
     public void mostrarCatalogo() {
         Scanner in = new Scanner(System.in);
+        int opcion;Scanner in = new Scanner(System.in);
         int opcion;
+        int desc;
+        String option;
+        int more;
+        LinkedList<Producto> carrito = new LinkedList<Producto>();
         Catalogo catalogo = new Catalogo();
         Iterator productos = catalogo.getIterador();
         System.out.println("**********BIENVENIDO A CHEEMSMART**********");
