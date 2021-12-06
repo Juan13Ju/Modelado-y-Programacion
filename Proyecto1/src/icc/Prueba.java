af64711d8aecead7478a79efe930bce40002029c
SergioMjCb <30817641+SergioMjCb@users.noreply.github.com>
2021-12-05 18:44:05 -0600

    Add files via upload

---
+++ Proyecto1/src/icc/Prueba.java
@@ -0,0 +1,30 @@
+package icc;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Prueba {
+private static final Random random = new Random();
+
+public static void main(final String[] args) {
+
+    System.out.println("¡Bienvenido al Simulador para el Lanzamiento de Dados!");
+    System.out.println("¿De cuantos lanzamientos consiste tu simulacion?");
+    int entrada = 0;
+    try (Scanner scanner = new Scanner(System.in)) {
+        entrada = scanner.nextInt();
+    }
+    final int[] simulacionDados = new int[11];
+    for (int x = 0; x < entrada; x++) {
+        simulacionDados[amount() - 2]++;
+    }
+    System.out.println(String.format("RESULTADOS DE LA SIMULACION"));
+    for (int i = 0; i < 11; i++) {
+        System.out.println(String.format("%s salio un total de %s veces ", i + 2, simulacionDados[i]));
+    }
+}
+
+    public static int amount() {
+        return random.nextInt(6) + random.nextInt(6) + 2;
+
+    }
+}
